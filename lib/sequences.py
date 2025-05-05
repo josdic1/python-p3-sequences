a1cdc551491f0f9459867fc0fc74495da7892ab8
josdic1 <joshuadicker@gmail.com>
2025-05-05 13:15:43 -0400

    sequence lab completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    sequence = []
+    if length == 0:
+        print(sequence)
+        return
+    if length == 1:
+        sequence = [0]
+        print(sequence)
+        return
+
+    sequence = [0, 1]
+
+    while len(sequence) < length:
+        next_number = sequence[-1] + sequence[-2]
+        sequence.append(next_number)
+    
+    print(sequence)
